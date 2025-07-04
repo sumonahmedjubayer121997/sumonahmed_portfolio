96470ef208b23619d40f2c9d87b40b6991ded1a3
sumonahmedjubayer121997 <jubayersumonahmed@gmail.com>
2025-07-04 02:08:50 +0100

    updated

--- src/components/FluidCursor.tsx
+++ src/components/FluidCursor.tsx
@@ -1,8 +1,7 @@
-
 "use client";
 import { useEffect } from "react";
 
-import fluidCursor from "@/hooks/use-FluidCursor";
+import fluidCursor from "@/hooks/use-fluidCursor";
 
 const FluidCursor = () => {
   useEffect(() => {
