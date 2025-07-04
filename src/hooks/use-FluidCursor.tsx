8b36e3ebf82c4f1e7ae6733ac09ab87b79098d00
sumonahmedjubayer121997 <jubayersumonahmed@gmail.com>
2025-07-04 02:17:28 +0100

    nthing

--- src/hooks/use-FluidCursor.tsx
+++ src/hooks/use-FluidCursor.tsx
@@ -1252,9 +1252,9 @@ const useFluidCursor = () => {
 
   function generateColor() {
     let c = HSVtoRGB(Math.random(), 1.0, 1.0);
-    c.r *= 0.05;
-    c.g *= 0.05;
-    c.b *= 0.05;
+    c.r *= 0.15;
+    c.g *= 0.15;
+    c.b *= 0.15;
     return c;
   }
 
