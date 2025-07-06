f9bda170a34d430ffdaa045107446c1143a9a0ca
Sumon Ahmed <jubayersumonahmed@gmail.com>
2025-07-06 11:46:39 +0000

    Changed email bg color

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -164,7 +164,7 @@ const ProfileSection = () => {
     </button>
   </a>
   <a href="mailto:sumonahmedjubayer@gmail.com">
-    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-gray-600 bg-primary shadow hover:bg-primary/90 h-9 px-4 py-2 text-white hover:text-oldsilver">
+    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-gray-600 bg-neutral-500 shadow hover:bg-primary/90 h-9 px-4 py-2 text-white hover:text-oldsilver">
       <span className="text-sm pr-2">
         <svg
           stroke="currentColor"
